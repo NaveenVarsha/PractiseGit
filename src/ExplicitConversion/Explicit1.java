d9196b8a2e460d48aaa41c6e028f7815ca0a40c4
NaveenVarsha <naveentesting1324@gmail.com>
2024-04-27 18:45:49 +0530

    Commit message

---
+++ src/ExplicitConversion/Explicit1.java
@@ -0,0 +1,16 @@
+package ExplicitConversion;
+
+public class Explicit1 {
+
+	public static void main(String[] args) {
+		
+		long a = 10;
+		int b;
+		
+		
+		b=(int)a;
+		System.out.println(b);
+
+	}
+
+}
---
+++ src/implicitConversion/Implicit1.java
@@ -0,0 +1,16 @@
+package implicitConversion;
+
+public class Implicit1 {
+
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		long b ;
+		
+		b=a;
+		System.out.println("The valoue of b" + b);
+		
+
+	}
+
+}
